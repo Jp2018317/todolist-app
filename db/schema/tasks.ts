7734e20b7f841841d06cbd8d13390ef0aa7a94a9
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-30 17:01:51 -0600

    fix: status type from text to text enum

--- db/schema/tasks.ts
+++ db/schema/tasks.ts
@@ -5,7 +5,7 @@ import { users } from "./user";
 export const tasks = sqliteTable('tasks', {
   id: integer("id").primaryKey({ autoIncrement: true }),
   title: text('text').unique().notNull(),
-  status: text('text').notNull(),
+  status: text('text', { enum: ["Complete", "Incomplete"] }).notNull(),
   createdAt: text('createdAt').default(sql`CURRENT_TIMESTAMP`).notNull(),
   updatedAt: text('updatedAt'),
   user: text("user").references(() => users.username)
