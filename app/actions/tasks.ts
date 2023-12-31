be50bd087de90dd0b6e5c4c7dafda75f5547603d
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-31 14:20:09 -0600

    fix: return only the user logged tasks

--- app/actions/tasks.ts
+++ app/actions/tasks.ts
@@ -5,18 +5,18 @@ import { db } from "@/db"
 import { tasks } from "@/db/schema/tasks";
 
 //Drizzle
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { datetime } from "drizzle-orm/mysql-core";
 
 //Types
 import { Task } from "@/config/types";
 
-export async function getTasks() {
-    return db.select().from(tasks);
+export async function getTasks(username: string) {
+    return db.select().from(tasks).where(eq(tasks.author, username));
 }
 
-export async function getTasksByStatus(status: "Complete" | "Incomplete") {
-    return db.select().from(tasks).where(eq(tasks.status, status));
+export async function getTasksByStatus(status: "Complete" | "Incomplete", username: string) {
+    return db.select().from(tasks).where(sql`${tasks.status} = ${status} and ${tasks.author} = ${username}`);
 }
 
 export async function addTask({title, author}: Omit<Task, "id" | "status" | "createdAt" | "updatedAt">){
