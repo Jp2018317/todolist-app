55da4c555447696ad0e7fabb8bad6df20bd42af6
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-31 14:40:44 -0600

    feat: Tasks reusable component

---
+++ app/components/TaskBox.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+//types
+import { Task } from "@/config/types";
+
+//icons
+import { FaTrash } from "react-icons/fa6";
+import { MdModeEdit } from "react-icons/md";
+
+export default function TaskBox({
+  id,
+  title,
+  status,
+  createdAt,
+  updatedAt,
+}: Omit<Task, "author">) {
+  return (
+    <div className="w-full p-3 flex items-center justify-between gap-4 bg-white rounded-md">
+      <div className="w-full flex items-center gap-3">
+        <input
+          type="checkbox"
+          checked={status === "Complete" ? true : false}
+          className="w-6 h-6 text-indigo-600 bg-gray-100 border-gray-300 rounded focus:ring-indigo-500"
+        />
+        <div className="flex flex-col justify-center">
+          <span className="font-medium text-gray-700">{title}</span>
+          <span className="text-xs text-gray-500">
+            Created: {createdAt} {updatedAt && `| Updated: ${updatedAt}`}
+          </span>
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-3">
+        <button
+          type="button"
+          className="bg-gray-200 p-2 rounded-md text-gray-700 hover:text-red-500"
+        >
+          <FaTrash />
+        </button>
+        <button
+          type="button"
+          className="bg-gray-200 p-2 rounded-md text-gray-700 hover:text-indigo-600"
+        >
+          <MdModeEdit />
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/TasksView.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+//react
+import { useEffect, useState } from "react";
+
+//types
+import { Task } from "@/config/types";
+import { getTasks } from "../actions/tasks";
+
+//icons
+import TaskBox from "./TaskBox";
+
+export default function TasksView() {
+  const [tasks, setTasks] = useState<Task[] | null>(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const allTasks = await getTasks("jmorales317");
+        setTasks(allTasks);
+      } catch (error) {
+        setTasks([]);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className="w-full h-full flex flex-col space-y-4 justify-center items-center bg-indigo-50 rounded-lg min-h-20 mt-2 p-5">
+      {tasks ? (
+        tasks.map((task) => (
+          <TaskBox
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            status={task.status}
+            createdAt={task.createdAt}
+            updatedAt={task.updatedAt}
+          />
+        ))
+      ) : (
+        <div className="bg-zinc-200 px-3 py-1.5 rounded-md text-sm font-medium">
+          No Todo Found
+        </div>
+      )}
+    </div>
+  );
+}
