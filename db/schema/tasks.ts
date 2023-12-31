0a0844d836d12bb45e751e4ec568e5500c8edb37
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-31 07:56:55 -0600

    fix: schemas syntax error fixed

--- db/schema/tasks.ts
+++ db/schema/tasks.ts
@@ -4,9 +4,9 @@ import { users } from "./user";
 
 export const tasks = sqliteTable('tasks', {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  title: text('text').unique().notNull(),
-  status: text('text', { enum: ["Complete", "Incomplete"] }).notNull(),
+  title: text('title').unique().notNull(),
+  status: text('status', { enum: ["Complete", "Incomplete"] }).default("Incomplete").notNull(),
   createdAt: text('createdAt').default(sql`CURRENT_TIMESTAMP`).notNull(),
   updatedAt: text('updatedAt'),
-  user: text("user").references(() => users.username)
+  author: text("author").references(() => users.username).notNull(),
 });
--- db/schema/user.ts
+++ db/schema/user.ts
@@ -3,7 +3,7 @@ import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('users', {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  username: text('text').unique().notNull(),
-  password: text('text').notNull(),
-  createdAt: text('createdAt').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  username: text("username").unique().notNull(),
+  password: text("password").notNull(),
+  createdAt: text("createdAt").default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
