440e6ebb37de1072737899e469ca6f330d171777
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-31 15:02:34 -0600

    fix: status parameter in addNewTask

--- app/actions/tasks.ts
+++ app/actions/tasks.ts
@@ -19,9 +19,10 @@ export async function getTasksByStatus(status: "Complete" | "Incomplete", userna
     return db.select().from(tasks).where(sql`${tasks.status} = ${status} and ${tasks.author} = ${username}`);
 }
 
-export async function addNewTask({title, author}: Omit<Task, "id" | "status" | "createdAt" | "updatedAt">){
+export async function addNewTask({title, status, author}: Omit<Task, "id" | "createdAt" | "updatedAt">){
     await db.insert(tasks).values({
         title: title,
+        status: status,
         author: author,
     });
 }
