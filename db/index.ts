31bfd01bcb973fc281bad3a3ad3e64cab1027c37
jmorales <jmorales-2018317@kinal.edu.gt>
2023-12-31 00:03:44 -0600

    fix: env variables imported from config.ts

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+import { DATABASE_AUTH_TOKEN, DATABASE_URL } from '@/config';
 
-const client = createClient({ url: process.env.DATABASE_URL!, authToken: process.env.DATABASE_AUTH_TOKEN });
+const client = createClient({ url: DATABASE_URL!, authToken: DATABASE_AUTH_TOKEN });
 
 export const db = drizzle(client);
