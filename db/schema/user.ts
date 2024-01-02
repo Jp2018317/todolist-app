853d190c8b55714a8fd221750e10d68795cbc33e
jmorales <jmorales-2018317@kinal.edu.gt>
2024-01-02 14:22:47 -0600

    fix: delete default from createdAt parameter in user schema

--- db/schema/user.ts
+++ db/schema/user.ts
@@ -1,9 +1,8 @@
-import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('users', {
   id: integer("id").primaryKey({ autoIncrement: true }),
   username: text("username").unique().notNull(),
   password: text("password").notNull(),
-  createdAt: text("createdAt").default(sql`CURRENT_TIMESTAMP`).notNull(),
+  createdAt: text("createdAt").notNull(),
 });
