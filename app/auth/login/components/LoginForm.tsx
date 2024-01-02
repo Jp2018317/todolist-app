fe04b7df3949f1897a168d47ea566e49c03e99df
jmorales <jmorales-2018317@kinal.edu.gt>
2024-01-02 16:23:55 -0600

    feat: loading states

--- app/auth/login/components/LoginForm.tsx
+++ app/auth/login/components/LoginForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+//react
+import { useState } from "react";
+
 //actions
 import { login } from "@/app/actions/users";
 import { useRouter } from "next/navigation";
 
-//react
-import { useState } from "react";
+//icons
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function LoginForm() {
   //Login Values
@@ -14,16 +17,19 @@ export default function LoginForm() {
     password: string;
   }>({ username: "", password: "" });
 
-  //Login handle error
+  //Login handle error & loading state
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const router = useRouter();
 
-  async function userLogin(){
+  async function userLogin() {
+    setIsLoading(true);
     const userLogged = await login(loginValues.username, loginValues.password);
-    if(userLogged.length === 0){
-        setError(true);
-        return;
+    if (userLogged.length === 0) {
+      setError(true);
+      setIsLoading(false);
+      return;
     }
     setError(false);
     localStorage.setItem("user", loginValues.username);
@@ -62,13 +68,20 @@ export default function LoginForm() {
       </div>
       <div className="w-full flex flex-col gap-2 mt-4 mb-2">
         <button
-          onClick={(e) => {e.preventDefault(); userLogin()}}
+          onClick={(e) => {
+            e.preventDefault();
+            userLogin();
+          }}
           disabled={
             loginValues.username.length < 2 || loginValues.password.length < 8
           }
-          className="w-full bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-400 disabled:bg-indigo-300 transition-colors duration-100 text-white px-5 py-2 rounded-md font-medium"
+          className="w-full flex justify-center bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-400 disabled:bg-indigo-300 transition-colors duration-100 text-white px-5 py-2 rounded-md font-medium"
         >
-          Login
+          {isLoading ? (
+            <AiOutlineLoading3Quarters className="animate-spin w-6 h-6" />
+          ) : (
+            "Login"
+          )}
         </button>
         {error && (
           <span className="text-xs text-red-500 font-medium text-center">
