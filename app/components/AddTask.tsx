cbdf7e244c409bf0f9d16b9a9c231fb387c2e7e8
jmorales <jmorales-2018317@kinal.edu.gt>
2024-01-01 18:50:10 -0600

    feat: status dropdown for add task modal

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -9,20 +9,24 @@ import { FaAngleDown } from "react-icons/fa";
 
 //actions
 import { addNewTask } from "../actions/tasks";
-import { ListFilter } from "@/config/config";
+import { listFilter, statusDropdown } from "@/config/config";
 
 
 export default function AddTask() {
-  //
-  const [showAddTask, setShowAddTask] = useState(false);
+  //Filter
   const [openFilter, setOpenFilter] = useState(false);
+  const [filterValue, setFilterValue] = useState("ALL");
 
-  const [taskFilter, setTaskFilter] = useState("ALL");
-  const [taskValue, setTaskValue] = useState({title: "", status: "Incomplete"});
+  //Add Task Modal
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [statusFilter, setStatusFilter] = useState(false);
+  const [taskValue, setTaskValue] = useState<{title: string, status: "Complete" | "Incomplete"}>({title: "", status: "Incomplete"});
 
   async function newTask() {
     console.log("añadir task");
-    await addNewTask({title: taskValue.title, status: "Incomplete", author: "jmorales317"});
+    console.log({TASKS_VALUES: taskValue})
+    await addNewTask({title: taskValue.title, status: taskValue.status, author: "jmorales317"});
+    setTaskValue({title: "", status: "Incomplete"})
     setShowAddTask(false);
   }
 
@@ -38,7 +42,7 @@ export default function AddTask() {
         <div className="relative">
           <button onClick={() => setOpenFilter(!openFilter)} className="bg-gray-300 hover:bg-gray-400 active:bg-gray-200 flex justify-between items-center gap-x-8 px-5 py-2 rounded-md font-medium">
             <div>
-              { taskFilter }
+              { filterValue }
             </div>
             <FaAngleDown />
           </button>
@@ -46,10 +50,10 @@ export default function AddTask() {
             openFilter && (
               <div className="absolute w-full right-0 mt-2 bg-indigo-50 border border-indigo-200 flex flex-col justify-between items-center rounded-md font-medium">
                 {
-                  ListFilter.map((filter) => (
+                  listFilter.map((filter) => (
                     <button
                       key={filter}
-                      onClick={() => { setTaskFilter(`${filter}`); setOpenFilter(!openFilter) }}
+                      onClick={() => { setFilterValue(`${filter}`); setOpenFilter(!openFilter) }}
                       className="w-full hover:bg-indigo-100 active:bg-indigo-200 border-b border-indigo-200 text-left gap-x-2 px-4 py-1.5 font-medium"
                     >
                       {filter}
@@ -65,10 +69,10 @@ export default function AddTask() {
         <>
           <div
             onClick={() => setShowAddTask(false)}
-            className="absolute z-30 top-0 left-0 flex justify-center items-center w-full h-[100dvh] bg-black/50"
+            className="fixed z-30 top-0 left-0 flex justify-center items-center w-full h-[100dvh] bg-black/50"
           />
 
-          <div className="absolute top-1/3 left-1/2 -translate-x-1/2 z-50 w-full max-w-md bg-indigo-50 rounded-lg p-5 space-y-6">
+          <div className="fixed top-1/3 left-1/2 -translate-x-1/2 z-50 w-full max-w-md bg-indigo-50 rounded-lg p-5 space-y-6">
             <h2 className="text-xl font-semibold text-gray-600">Add Task</h2>
             <div className="flex flex-col gap-2">
               <span className="text-sm text-gray-500 font-medium">Title</span>
@@ -78,23 +82,48 @@ export default function AddTask() {
                 type="text"
                 className="h-10 p-2"
               />
+              {
+                taskValue.title.length < 3 && (
+                  <span  className="text-xs text-red-500 font-medium">Title must have more than 3 characters</span>
+                )
+              }
               <span className="text-sm text-gray-500 font-medium">Status</span>
-              <input 
-                defaultValue={taskValue.status}
-                onChange={(e) => setTaskValue({ ...taskValue, status: e.target.value})}
-                type="text"
-                className="h-10 p-2"
-              />
+              <div className="relative">
+                <button onClick={() => setStatusFilter(!statusFilter)} className="w-full bg-white flex justify-between items-center gap-x-8 px-5 py-2 rounded-md font-medium">
+                  <div>
+                    { taskValue.status }
+                  </div>
+                  <FaAngleDown />
+                </button>
+                {
+                  statusFilter && (
+                    <div className="absolute w-full right-0 bg-indigo-50 border border-indigo-200 flex flex-col justify-between items-center rounded-sm font-medium">
+                      {
+                        statusDropdown.map((status) => (
+                          <button
+                            key={status}
+                            onClick={() => { setTaskValue({...taskValue, status: status === "Complete" ? "Complete" : "Incomplete"}); setStatusFilter(!statusFilter) }}
+                            className="w-full hover:bg-indigo-100 active:bg-indigo-200 border-b border-indigo-200 text-left gap-x-2 px-4 py-1.5 font-medium"
+                          >
+                            {status}
+                          </button>
+                        ))
+                      }
+                    </div>
+                  )
+                }
+              </div>
             </div>
             <div className="w-full flex gap-2">
               <button
                 onClick={() => newTask()}
-                className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-400 transition-colors duration-100 text-white px-5 py-2 rounded-md font-medium"
+                disabled={taskValue.title.length < 3}
+                className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-400 disabled:bg-indigo-300 transition-colors duration-100 text-white px-5 py-2 rounded-md font-medium"
               >
                 Add Task
               </button>
               <button
-                onClick={() => setShowAddTask(false)}
+                onClick={() => {setShowAddTask(false); setTaskValue({title: "", status: "Incomplete"})}}
                 className="bg-gray-300 hover:bg-gray-400 active:bg-gray-200 transition-colors duration-100 text-gray-600 px-5 py-2 rounded-md font-medium"
               >
                 Cancel
